a91b63ff7f2ab57d4e74446c741fac140a66e8ba
Donnie Adams <donnie@thedadams.com>
2024-05-20 10:05:03 -0400

    fix: actually add init.vim
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ config_dir=${XDG_CONFIG_HOME:-~/.config}
 for f in $(ls); do
     if [[ $f == "init.vim" ]]; then
         rm -rf $config_dir/nvim/init.vim
+        mkdir -p $config_dir/nvim
         ln -s $pwd/$f $config_dir/nvim/init.vim
     elif [[ $f != "install.sh" && $f != "sbdiff.sh" ]]; then
         rm -rf $HOME/.$f
