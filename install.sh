18c5f3f80494b223ecaf183b6ffdc417165cc79c
Donnie Adams <donnie@obot.ai>
2025-11-29 16:10:21 -0500

    chore: add zellij and simplify configuration
    Signed-off-by: Donnie Adams <donnie@obot.ai>

--- install.sh
+++ install.sh
@@ -6,15 +6,11 @@ pwd=`pwd`
 config_dir=${XDG_CONFIG_HOME:-~/.config}
 
 for f in $(ls); do
-    if [[ $f == "zed-settings.json" ]]; then
-        rm -rf $config_dir/zed/settings.json
-        mkdir -p $config_dir/
-        ln -s $pwd/$f $config_dir/zed/settings.json
-    elif [[ $f == "nvim" || $f == "helix" || $f == "jj" || $f == "ghostty" || $f == "fresh" || $f == "rio" ]]; then
+    if [[ $f == "nvim" || $f == "jj" || $f == "zellij" || $f == "zed" ]]; then
         rm -rf $config_dir/$f
         mkdir -p $config_dir/
         ln -s $pwd/$f $config_dir/$f
-    elif [[ $f != "install.sh" && $f != "sbdiff.sh" ]]; then
+    elif [[ $f != "install.sh" ]]; then
         rm -rf $HOME/.$f
         ln -s $pwd/$f $HOME/.$f
     fi
